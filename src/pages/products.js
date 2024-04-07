a1ced5a18910549bbfb43a3a37fbb6c82e71baa2
Hassan Zabatt <hassan.zabatt@epitech.eu>
2024-04-07 09:11:20 +0200

    Updating the real product page

--- src/pages/products.js
+++ src/pages/products.js
@@ -4,6 +4,28 @@ import Layout from "../components/layout"
 
 const ProductsPage = () => (
   <Layout>
+    <div style={{
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: `var(--teal)`, // Set the image as background
+      backgroundSize: 'cover', // Cover the entire div
+      backgroundPosition: 'center', // Center the background image
+      color: 'var(--white)',
+      minHeight: 'calc(100vh - 80px)', // Subtract the height of the navbar
+      padding: '1rem'
+    }}>
+      <h1 style={{ textShadow: '2px 2px 4px rgba(0, 0, 0, 0.5)', fontSize: '3em' }}>Discover our products</h1>
+      <p style={{ textShadow: '2px 2px 4px rgba(0, 0, 0, 0.5)', fontSize: '1.5em' }}>
+        Let's find out what you love !
+      </p>
+      <div style={{ display: 'flex', justifyContent: 'center' }}>
+        <p style={{ fontSize: '1.2em', maxWidth: '60%', textAlign: 'center' }}>
+          Explore our diverse range of carpets, each offering a unique blend of style and comfort. From our premium luxury collections to our affordable everyday options, there's a perfect Elenfaloth Carpet for every home and budget.
+        </p>
+      </div>
+    </div>
     <div style={{ margin: '0 auto', maxWidth: 960, padding: '2rem' }}>
       <h1>Our Products</h1>
       <ul>
